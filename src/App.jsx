8c340eca9437dec7c0769efc8bf30f59b8a27894
nitinkr21nv <nitin2111kr@gmail.com>
2026-04-10 13:23:50 +0530

    added

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import SearchBar from "./components/SearchBar";
 import MovieList from "./components/MovieList";
 import MovieModal from "./components/MovieModal";
 
-const API_KEY = "import.meta.env.VITE_API_KEY";
+const API_KEY = import.meta.env.VITE_API_KEY;
 
 function App() {
   const [movies, setMovies] = useState([]);
